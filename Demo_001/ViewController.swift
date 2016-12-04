0f6f6289db3cc07e20d99c7196d44fe76f0fbdf8
dragonshu <dragonshu@dragonshudeimac.lan>
2016-12-05 00:21:13 +0800

    Init: Commit

---
+++ Demo_001/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Demo_001
+//
+//  Created by dragonshu on 2016/12/5.
+//  Copyright © 2016年 dragonshu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var a = 1
+        var b = 2
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
