d43bf7ed8232fb58a543330ef44ca942adc2366b
dragonshu <dragonshu@dragonshudeiMac.lan>
2016-12-10 19:20:56 +0800

    third

--- Demo_001/ViewController.swift
+++ Demo_001/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
         
         var a = 1
         var b = 2
+        var c=3
+        var d=4
+        var e=5
     }
 
     override func didReceiveMemoryWarning() {
